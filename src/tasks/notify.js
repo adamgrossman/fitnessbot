b5db3f0f8050ba80ba1a993a51b92d1d98ca3733
Adam <adam.grossman08@me.com>
2017-09-17 16:44:26 -0700

    Notifications working

---
+++ src/tasks/notify.js
@@ -0,0 +1,78 @@
+'use strict'
+
+var _ = require('lodash')
+var config = require('../config')
+var trending = require('github-trending')
+var Botkit = require('botkit')
+var moment = require('moment-timezone')
+
+var controller = Botkit.slackbot({})
+var bot = controller.spawn()
+
+bot.configureIncomingWebhook({ url: config('WEBHOOK_URL') })
+// bot.configureIncomingWebhook({ url: 'https://hooks.slack.com/services/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' })
+
+var msgDefaults = {
+  response_type: 'in_channel',
+  username: 'testfit',
+  icon_emoji: config('ICON_EMOJI')
+}
+
+var activeHours = ['9', '10', '11', '12', '14', '15', '16']
+var today = moment().tz('America/Los_Angeles')
+var hourOfDay = today.format('H')
+
+function notificationValid() {
+  if (today.weekday() === 0 || today.weekday() === 6) {
+    return false
+  } else if (activeHours.indexOf(hourOfDay) === -1) {
+    return false
+  }
+  return true
+}
+
+function determineGreeting() {
+  var greetings = [
+    'Hey there! It\'s time for... ',
+    'Me again! How about... ',
+    'You\'re gonna hate me, but... ',
+    'I\'ve got a fun one for you! Let\'s do... ',
+    'Yup, you guessed it... '
+  ]
+  var greeting = ''
+  switch(hourOfDay) {
+    case '9':
+      greeting = 'Good morning! Let\'s get this day started with... '
+    case '14':
+      greeting = 'Time to work off that lunch... '
+    case '16':
+      greeting = 'Last one! Let\'s do... '
+    default:
+      greeting = greetings[Math.floor(Math.random() * greetings.length)]
+  }
+  return greeting
+}
+
+function determineExercise() {
+  var exercises = [
+    '20 push-ups (2 sets of 10 each).',
+    '20 squats.',
+    'a 1 minute plank.',
+    '10 tricep dips.',
+    '20 calf raises (2 sets of 10 each).',
+    '20 lunges (10 for each leg).'
+  ]
+  return exercises[Math.floor(Math.random() * exercises.length)]
+}
+
+trending('javascript', (err, repos) => {
+  if (err) throw err
+
+  if (notificationValid()) {
+    var text = determineGreeting() + determineExercise()
+    let msg = _.defaults({ "text": text }, msgDefaults)
+    bot.sendWebhook(msg, (err, res) => {
+      if (err) throw err
+    })
+  }
+})
